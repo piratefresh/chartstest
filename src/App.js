0016773f9c2d9f8161078296868de981e68062c7
Magnus Nilsen <magnussithnilsen@gmail.com>
2019-02-13 09:44:30 -0500

    multiline

--- src/App.js
+++ src/App.js
@@ -1,65 +1,122 @@
 import React, { Component } from "react";
 import "./App.css";
 import FusionCharts from "fusioncharts/core";
-import Line2D from "fusioncharts/viz/line";
+import MultiLine2D from "fusioncharts/viz/msline";
 import FusionTheme from "fusioncharts/themes/es/fusioncharts.theme.fusion";
 import ReactFC from "react-fusioncharts";
-ReactFC.fcRoot(FusionCharts, Line2D, FusionTheme);
+ReactFC.fcRoot(FusionCharts, MultiLine2D, FusionTheme);
 
 const chartConfigs = {
-  type: "line", // The chart type
+  type: "msline", // The chart type
   width: "700", // Width of the chart
   height: "400", // Height of the chart
   dataFormat: "json", // Data type
   dataSource: {
     // Chart Configuration
     chart: {
-      caption: "Total footfall in Bakersfield Central",
-      subCaption: "Last week",
+      caption: "Number of visitors last week",
+      subCaption: "Bakersfield Central vs Los Angeles Topanga",
       xAxisName: "Day",
-      yAxisName: "No. of Visitors",
-      lineThickness: "2",
       theme: "fusion"
     },
-    data: [
+    categories: [
       {
-        label: "Mon",
-        value: "15123"
+        category: [
+          {
+            label: "Mon"
+          },
+          {
+            label: "Tue"
+          },
+          {
+            label: "Wed"
+          },
+          {
+            vline: "true",
+            lineposition: "0",
+            color: "#6baa01",
+            labelHAlign: "center",
+            labelPosition: "0",
+            label: "National holiday",
+            dashed: "1"
+          },
+          {
+            label: "Thu"
+          },
+          {
+            label: "Fri"
+          },
+          {
+            label: "Sat"
+          },
+          {
+            label: "Sun"
+          }
+        ]
+      }
+    ],
+    dataset: [
+      {
+        seriesname: "Bakersfield Central",
+        data: [
+          {
+            value: "15123"
+          },
+          {
+            value: "14233"
+          },
+          {
+            value: "25507"
+          },
+          {
+            value: "9110"
+          },
+          {
+            value: "15529"
+          },
+          {
+            value: "20803"
+          },
+          {
+            value: "19202"
+          }
+        ]
       },
       {
-        label: "Tue",
-        value: "14233"
-      },
-      {
-        label: "Wed",
-        value: "23507"
-      },
-      {
-        label: "Thu",
-        value: "9110"
-      },
-      {
-        label: "Fri",
-        value: "15529"
-      },
-      {
-        label: "Sat",
-        value: "20803"
-      },
-      {
-        label: "Sun",
-        value: "19202"
+        seriesname: "Los Angeles Topanga",
+        data: [
+          {
+            value: "13400"
+          },
+          {
+            value: "12800"
+          },
+          {
+            value: "22800"
+          },
+          {
+            value: "12400"
+          },
+          {
+            value: "15800"
+          },
+          {
+            value: "19800"
+          },
+          {
+            value: "21800"
+          }
+        ]
       }
     ],
     trendlines: [
       {
         line: [
           {
-            startvalue: "18500",
-            color: "#1aaf5d",
-            displayvalue: "Average{br}weekly{br}footfall",
+            startvalue: "17022",
+            color: "#6baa01",
             valueOnRight: "1",
-            thickness: "2"
+            displayvalue: "Average"
           }
         ]
       }
